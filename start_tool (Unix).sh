cc83c77272d2480cd1985edf69c0e67e4ddbdeea
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2022-11-18 10:17:12 -0500

    Edited start_tool for Unix and added one for Windows

--- start_tool (Unix).sh
+++ start_tool (Unix).sh
@@ -1,10 +1,8 @@
-# npm i
-# cd scanner
-# npm i
-# cd ..
+npm i
+cd scanner
+npm i
+cd ..
 node cli/welcome.js
-# node cli/start_scanner.js
 node scanner/dist/index.js &
 node cli/start_webapp.js
-# echo '\n'
 npx serve webapp
